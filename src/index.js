edc23d8bab728e3ee9e54cc09fee55511176b42b
linhao <2218431632@qq.com>
2022-08-30 11:45:43 +0800

    创建项目

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const bookList = [
+  {
+    img: "./logo192.png",
+    bookName: "西游记",
+    author: "林浩",
+  },
+  {
+    img: "./logo192.png",
+    bookName: "水浒传",
+    author: "哈哈哈",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const bookItem = bookList.map((prop) => {
+  return (
+    <Book img={prop.img} bookName={prop.bookName} author={prop.author}></Book>
+  );
+});
+
+function BookList() {
+  return <section className="booklist">{bookItem}</section>;
+}
+
+function Book({ img, bookName, author }) {
+  return (
+    <div className="book">
+      <Image img={img}></Image>
+      <BookName bookName={bookName}></BookName>
+      <Author author={author}></Author>
+    </div>
+  );
+}
+
+const Image = ({ img }) => {
+  return (<img src={img}></img>);
+};
+
+function BookName({ bookName }) {
+  return (<h4>{bookName}</h4>);
+}
+
+function Author({ author }) {
+  return (<h4>{author}</h4>);
+}
+
+ReactDOM.render(<BookList/>, document.getElementById("root"));
